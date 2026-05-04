dccb19f0e3f18c3a46afb2ebb5fc7fee1338e1d5
jamsnxs <jailsonhh72c@gmail.com>
2026-05-04 15:41:04 -0300

    Adiciona código do Arduino para os sensores DHT22 e HW-080

---
+++ arduino/main/main.ino
@@ -0,0 +1,43 @@
+#include "DHT.h"
+
+#define SENSOR_PIN A0
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  pinMode(SENSOR_PIN, INPUT);
+}
+
+void loop() {
+  // Sensor requer intervalo de leitura mínimo de 2 segundos
+  delay(2000);
+
+  float humidade = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+
+  if (isnan(humidade) || isnan(temperatura)) {
+    Serial.println(F("Falha na leitura do sensor DHT22"));
+    return;
+  }
+
+  unsigned int sensorValor = analogRead(SENSOR_PIN);
+  // converte intervalo de 10 bits (0 a  1023) para porcentagem proporcional
+  unsigned int porcentagem = (sensorValor * 100UL) / 1023;
+  // Unsigned long (UL) é usado na multiplicação para prevenir estouro de inteiro
+  // pois 1023 * 100 > 16 bits
+
+  Serial.println(F("---UMIDADE DO SOLO---"));
+  Serial.print(F("Output: "));
+  Serial.print(porcentagem);
+  Serial.println(F("%"));
+
+  Serial.println(F("---UMIDADE E TEMPERATURA DO AMBIENTE---"));
+  Serial.print(temperatura);
+  Serial.print(F(" ºC | Umidade: "));
+  Serial.print(humidade);
+  Serial.println(" %");
+}
